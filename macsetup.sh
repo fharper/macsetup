048f76419ea83a48c1952a5d6fcea46da72651ee
Frédéric Harper <fharper@oocz.net>
2018-11-20 16:16:06 -0500

    Reorder the b letters apps

--- macsetup.sh
+++ macsetup.sh
@@ -385,9 +385,6 @@ mas install 1405763798
 brew cask install battle-net
 open -a /usr/local/Caskroom/battle-net/latest/Battle.net-Setup.app
 
-#Battery Stats for AirPods
-mas install 1405763798
-
 #Boxer
 brew cask install boxer
 open -a Boxer
