2e11eb9e0528572c754c7ae2cc66d13e484885a4
Frédéric Harper <fharper@oocz.net>
2018-11-26 17:26:58 -0500

    Better header

--- macsetup.sh
+++ macsetup.sh
@@ -602,8 +602,9 @@ osascript -e 'tell application "System Events" to make login item at end with pr
 osascript -e 'tell application "System Events" to make login item at end with properties {name: "Todoist", path:"/Applications/Todoist.app", hidden:false}'
 osascript -e 'tell application "System Events" to make login item at end with properties {name: "TweetDeck", path:"/Applications/TweetDeck.app", hidden:false}'
 
-
-#Add apps in the Dock
+########################
+# Add apps in the Dock #
+########################
 dockutil --add /Applications/Google\ Chrome.app/ --allhomes
 dockutil --add /Applications/Visual\ Studio\ Code.app/ --allhomes
 dockutil --add /Applications/iTerm.app/ --allhomes
