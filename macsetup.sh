618b096ca2ded76a324c47f21baf0612dd17c3e8
Frédéric Harper <fharper@npmjs.com>
2018-12-18 13:33:19 -0500

    move NVM_DIR to bash profile (not sure why it was in this script)

--- macsetup.sh
+++ macsetup.sh
@@ -360,8 +360,6 @@ echo -e "\n"
 
 #Node stuff
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install v10.13.0
 npm install -g npm@latest
 npm config set prefix /usr/local
