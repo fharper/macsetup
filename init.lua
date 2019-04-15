0119de784308d0791a5eec0d5102acf2ef48e0a3
Frédéric Harper <fharper@npmjs.com>
2019-04-15 19:17:22 +0200

    add shortcut for my biography in small, medium and full versions

--- init.lua
+++ init.lua
@@ -31,6 +31,11 @@ local keywords = {
     ["fredslideshare"] = "https://www.slideshare.net/fredericharper",
     ["fredgoodread"] = "https://www.goodreads.com/fharper",
 
+    -- Biographies
+    ["fredbios"] = "Developer Advocate @ npmjs, former Mozilla & Microsoft one, Apress author, public speaker, #crazycatman & more",
+    ["fredbiom"] = "As a Senior Developer Advocate at npm, Frédéric Harper is helping developers being successful within the JavaScript world. Former Mozilla and Microsoft Technical Evangelist, Frédéric is a prolific international public speaker and the author of a book on personal branding published at Apress. Behind this extrovert who lives by the no bullshit policy is a very passionate individual who believes in the power of communication... and cat videos.",
+    ["fredbiol"] = "As a Senior Developer Advocate at npm, Frédéric Harper is helping developers being successful within the JavaScript world. Former Mozilla and Microsoft Technical Evangelist, he has had the pleasure to share his passion for technology at more than 140 events in about 20 countries. Capitalizing on his attention deficit and hyperactivity disorder (ADHD), Frédéric is always onto the next challenge. He is also the author of the “Success in Programming - How to Gain Recognition, Power, and Influence Through Personal Branding” book published at Apress. Behind this extrovert who lives by the no bullshit policy is a very passionate individual who believes in the power of communication... and cat videos.",
+    
     -- Emoticons
     [":flipp"] = "(╯°□°)╯︵ ┻━┻",
     [":shrug"] = "¯\\_(ツ)_/¯",
