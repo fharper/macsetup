358317d141984cfadc8f2cbe82d9d7709096c1b9
Frédéric Harper <fharper@npmjs.com>
2019-05-20 11:19:17 -0400

    make the font bigge

--- .hyper.js
+++ .hyper.js
@@ -9,7 +9,7 @@ module.exports = {
     updateChannel: 'stable',
 
     // default font size in pixels for all tabs
-    fontSize: 12,
+    fontSize: 14,
 
     // font family with optional fallbacks
     fontFamily: 'Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
