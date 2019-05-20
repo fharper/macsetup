cbd583cb7b5ac9c267a7218fe83746f9a9528a04
Frédéric Harper <fharper@npmjs.com>
2019-05-20 11:19:49 -0400

    remove font family fallback

--- .hyper.js
+++ .hyper.js
@@ -12,7 +12,7 @@ module.exports = {
     fontSize: 14,
 
     // font family with optional fallbacks
-    fontFamily: 'Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+    fontFamily: 'Menlo',
 
     // default font weight: 'normal' or 'bold'
     fontWeight: 'normal',
