191637320125c7a70107840dee15b595c32f8ef0
Frédéric Harper <fharper@oocz.net>
2019-08-21 16:25:16 -0400

    move git-lfs in the git section

--- macsetup.sh
+++ macsetup.sh
@@ -131,6 +131,7 @@ git config --global difftool.prompt false
 git config --global diff.tool vscode
 git config --global difftool.vscode.cmd 'code --diff --wait $LOCAL $REMOTE'
 
+brew install git-lfs
 gpg --list-secret-keys --keyid-format LONG
 echo "Copy & paste your public key UUID listed below (line sec - after the /)"
 read gpgUUID
@@ -139,7 +140,6 @@ git config --global commit.gpgsign true
 
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
 
-brew install git-lfs
 
 
 ######################
