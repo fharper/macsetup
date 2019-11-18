ce8729300545a15a3ffc59a2fd57050821e46ec4
Frédéric Harper <fharper@oocz.net>
2019-11-18 17:40:38 -0500

    switch to Oh My Zsh

--- macsetup.sh
+++ macsetup.sh
@@ -8,6 +8,9 @@ todo="\n########\n# TODO #\n########\n"
 # Install utils to run this script #
 ####################################
 
+#Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 #Brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew analytics off
