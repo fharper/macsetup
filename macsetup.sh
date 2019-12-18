bd2629cb51f88640e08b799aa3fe5c49a7ce8ea2
Frédéric Harper <fharper@oocz.net>
2019-12-18 18:19:26 -0500

    remove old comment

--- macsetup.sh
+++ macsetup.sh
@@ -172,7 +172,6 @@ open '/Users/fharper/Documents/mac/DigitalOcean - GoToWebinar/GoTo Opener.dmg'
 
 #NYC printer & scanner drivers
 open '/Users/fharper/Documents/mac/DigitalOcean - NYC Printer drivers/Dell C2665dnf Print Installer.pkg'
----open '/Users/fharper/Documents/mac/DigitalOcean - NYC Printer drivers/Dell C2665dnf Scan Installer.pkg'
 
 #Workspace ONE (SentinelOne)
 open '/Users/fharper/Documents/mac/DigitalOcean - WorkspaceONE/VMwareWorkspaceONEIntelligentHub.pkg'
