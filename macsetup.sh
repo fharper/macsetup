0495561668ec57a998832c0765f331ac1843f7fa
Frédéric Harper <fharper@oocz.net>
2020-01-16 14:40:24 -0500

    Remove Dropbox
    Moved to pCloud for different reasons

--- macsetup.sh
+++ macsetup.sh
@@ -78,8 +78,6 @@ osascript -e 'tell application "System Events" to make login item at end with pr
 brew cask install 1password
 dockutil --add /Applications/1Password\ 7.app/ --allhomes
 
-#Dropbox
-brew cask install dropbox
 #pCloud
 open '/Users/fharper/Documents/mac/pCloud 3.8.4/pCloud Drive 3.8.4.pkg'
 osascript -e 'tell application "System Events" to make login item at end with properties {name: "pCloud Drive", path:"/Applications/pCloud Drive.app", hidden:false}'
