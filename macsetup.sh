2dffb4da4b4fe2a56db1fc79c9578024a79d4579
Frédéric Harper <fharper@oocz.net>
2020-10-15 12:54:21 -0400

    open SQL files with Visual Studio Code

--- macsetup.sh
+++ macsetup.sh
@@ -770,6 +770,7 @@ duti -s org.videolan.vlc public.mp3 all #mp3
 duti -s net.kovidgoyal.calibre dyn.ah62d4rv4ge80c8x1gq all #Kindle ebooks
 duti -s org.videolan.vlc com.apple.m4a-audio all #M4A
 duti -s net.langui.WebPViewer public.webp all #WebP
+duti -s com.microsoft.VSCode dyn.ah62d4rv4ge81g6pq all #SQL
 
 ######################
 # Order apps in Dock #
