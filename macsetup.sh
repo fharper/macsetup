9f5658eeca1264f60da5128a3c70097090fe00b3
Frédéric Harper <hi@fred.dev>
2020-12-13 19:17:21 -0500

    open 3d CAD file with Preview

--- macsetup.sh
+++ macsetup.sh
@@ -798,6 +798,7 @@ duti -s com.apple.Preview org.openxmlformats.presentationml.presentation all #PP
 duti -s com.microsoft.VSCode public.css all #CSS
 duti -s com.microsoft.VSCode com.netscape.javascript-source all #JavaScript
 duti -s com.microsoft.VSCode public.ruby-script all #Ruby
+duti -s com.apple.Preview public.standard-tesselated-geometry-format all #3d CAD
 duti -s com.brave.Browser com.compuserve.gif all #gif
 
 ######################
