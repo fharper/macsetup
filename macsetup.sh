014d462a4fa3887e67f293583e45f8023a8bb819
Frédéric Harper <hi@fred.dev>
2020-12-21 14:32:14 -0500

    better header for Oh My Zsh

--- macsetup.sh
+++ macsetup.sh
@@ -655,6 +655,8 @@ loginitems -a Notion
 #
 # Oh My Zsh
 #
+# https://github.com/ohmyzsh/ohmyzsh
+#
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 #
