a3a27ab2136f1c55415d2f5ca5c68d78080da0a0
Frédéric Harper <hi@fred.dev>
2022-08-24 12:53:50 -0400

    add gh-pr-draft
    GitHub CLI Extension for converting PR to Draft

--- macsetup.sh
+++ macsetup.sh
@@ -1187,12 +1187,19 @@ brew install git-recent
 brew install git-sizer
 
 #
+# GitHub CLI & gh-pr-draft
+#
 # GitHub CLI
+# GitHub CLI Extension for converting PR to Draft
 #
 # https://github.com/cli/cli
+# https://github.com/kyanny/gh-pr-draft
 #
 brew install gh
 gh auth login
+gh config set editor "code --wait"
+gh config set git_protocol ssh --host github.com
+gh extension install kyanny/gh-pr-draft
 
 #
 # GoEnv + Go
