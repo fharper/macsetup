bf44a2c40bdf4d8456adfddcbf2c0e154805fa3f
Frédéric Harper <hi@fred.dev>
2022-08-24 12:44:20 -0400

    add instructions about Charles Proxy installation

--- macsetup.sh
+++ macsetup.sh
@@ -1049,6 +1049,7 @@ brew install bfg
 # https://www.charlesproxy.com
 #
 brew install --cask charles
+notification "Install Charles Proxy certificate in system & change to always trust"
 
 #
 # CocoaPods
