2358f5fadf1cb93f3fefc102c20ccb6ac9ad13e6
Frédéric Harper <hi@fred.dev>
2023-01-08 14:17:39 -0500

    reorder iterm on the Dock

--- macsetup.sh
+++ macsetup.sh
@@ -3143,9 +3143,9 @@ dockutil --move 'Mail' --position end --allhomes
 dockutil --move 'Notion Enhanced' --position end --allhomes
 dockutil --move 'Todoist' --position end --allhomes
 dockutil --move 'Slack' --position end --allhomes
-dockutil --move 'iTerm' --position end --allhomes
 dockutil --move 'Visual Studio Code' --position end --allhomes
 dockutil --move 'Pocket' --position end --allhomes
+dockutil --move 'iTerm' --position end --allhomes
 dockutil --move 'Spotify' --position end --allhomes
 dockutil --move '1Password' --position end --allhomes
 dockutil --move 'Photos' --position end --allhomes
