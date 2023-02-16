7b02f1fd27187d524abc221a1151cf22de4d97d1
Frédéric Harper <hi@fred.dev>
2023-02-16 16:49:10 -0500

    simplify the default application removal section

--- macsetup.sh
+++ macsetup.sh
@@ -983,29 +983,10 @@ fi
 #                      #
 ########################
 
-#
-# Garage Band
-#
 sudo rm -rf /Applications/GarageBand.app
-
-#
-# iMovie
-#
 sudo rm -rf /Applications/iMovie.app
-
-#
-# Keynote
-#
 sudo rm -rf /Applications/Keynote.app
-
-#
-# Numbers
-#
 sudo rm -rf /Applications/Numbers.app
-
-#
-# Pages
-#
 sudo rm -rf /Applications/Pages.app
 
 ################
