b374aa55dfe6863f0e0b52bd9ac70fc3c7f8798e
Frédéric Harper <hi@fred.dev>
2023-04-18 18:43:36 -0400

    fix Insta360 Link Controller installation

--- macsetup.sh
+++ macsetup.sh
@@ -3306,7 +3306,7 @@ installcask igdm
 #
 if [[ "$(isAppInstalled "Insta360 Link Controller")" = "false" ]]; then
     curl -L https://file.insta360.com/static/ff3fc5347835495dd970735b96db1097/Insta360LinkController_20230303_144839_signed_1677827088327.pkg --output Insta360LinkController.pkg
-    installPKGfromDMG Insta360LinkController.pkg
+    installPKG Insta360LinkController.pkg
 fi
 
 #
