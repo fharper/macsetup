d88e26c372b0c720346a07078f09aab1a14345ac
Frédéric Harper <hi@fred.dev>
2023-07-18 17:31:03 -0400

    add chaoskube Helm Chart repository

--- macsetup.sh
+++ macsetup.sh
@@ -2308,6 +2308,7 @@ if [[ "$(isCLAppInstalled helm)" = "false" ]]; then
     helm repo add portainer https://portainer.github.io/k8s
     helm repo add kubefirst https://charts.kubefirst.com
     helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+    helm repo add chaoskube https://linki.github.io/chaoskube/
     helm repo update
 fi
 
