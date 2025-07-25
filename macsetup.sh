6cc25d57e92c8f35f3fb9ec210bcb6d68ffcab4d
Frédéric Harper <hi@fred.dev>
2025-07-25 16:26:04 -0400

    update The Clock configuration information for the user

--- macsetup.sh
+++ macsetup.sh
@@ -1352,7 +1352,7 @@ if [[ "$(isAppInstalled "The Clock")" = "false" ]]; then
     installcask the-clock
     open -a "The Clock"
     getLicense "The Clock"
-    pausethescript "Before continuing, restore the settings of The Clock by going in 'Preferences' and select the tab 'Backup/Restore'. Click on the 'iCloud' checkbox, and click the 'Restore' button."
+    pausethescript "Before continuing, restore the settings of The Clock by going in 'Settings' and select the tab 'Backup/Restore'. Click on the 'iCloud' checkbox, and click the 'Restore' button."
 fi
 
 #
