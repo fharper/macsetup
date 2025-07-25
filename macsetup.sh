b7e34413481a9df37301bf5bb2e95d3b87ed2579
Frédéric Harper <hi@fred.dev>
2025-07-25 16:04:42 -0400

    move 1Password earlier in the installation process
    It's needed to connect to most applications or websites. I also split them in their own items.

--- macsetup.sh
+++ macsetup.sh
@@ -1088,6 +1088,50 @@ removeAppFromDock "TV"
 displaySection "Top Helper Applications"
 
 #
+# 1Password
+#
+# Password manager
+#
+# https://1password.com
+#
+# Notes: needed before 1Password CLI
+#
+if [[ "$(isAppInstalled 1Password)" = "false" ]]; then
+    installcask 1password
+    dockutil --add /Applications/1Password.app --allhomes
+fi
+
+#
+# 1Password CLI
+#
+# CLI for 1Password
+#
+# https://1password.com/downloads/command-line/
+#
+# Notes: needed before Alfred
+#
+if [[ "$(isCLAppInstalled op)" = "false" ]]; then
+    installkeg 1password-cli
+    open -a 1Password
+    pausethescript "Open 1Password settings, go to the "Developer" tab, and check 'Integrate with 1Password CLI'. After, press the 'Set up the SSH Agent' button before continuing"
+    restoreAppSettings ssh
+    eval $(op signin)
+fi
+
+#
+# 1Password Safari Extension
+#
+# 1Password  Safari integration
+#
+# https://apps.apple.com/us/app/1password-for-safari/id1569813296
+#
+if [[ "$(isAppInstalled "1Password for Safari")" = "false" ]]; then
+    installFromAppStore "1Password Safari Extension" 1569813296
+    open -a Safari
+    pausethescript "Do the require steps inside of Safari to activate 1Password"
+fi
+
+#
 # Alfred & alfred-google-translate & alfred-language-configuration
 #
 # Spotlight replacement
@@ -1650,33 +1694,6 @@ killall SystemUIServer
 #####################
 
 displaySection "Main applications"
-
-#
-# 1Password + 1Password CLI + git-credential-1password + Safari Extension
-#
-# Password manager
-# CLI for 1Password
-# Safari integration
-#
-# https://1password.com
-# https://1password.com/downloads/command-line/
-# https://apps.apple.com/us/app/1password-for-safari/id1569813296
-#
-if [[ "$(isAppInstalled 1Password)" = "false" ]]; then
-    installcask 1password
-    giveScreenRecordingPermission 1Password
-    dockutil --add /Applications/1Password.app --allhomes
-    installkeg 1password-cli
-    open -a 1Password
-    pausethescript "Open 1Password settings, and check 'Connect with 1Password CLI' & click the 'Set up SSH Agent' button in the 'Developer' tab before continuing"
-    restoreAppSettings ssh
-    eval $(op signin)
-    pausethescript "Sign in to 1Password before continuing"
-    brew tap develerik/tools
-    installFromAppStore "1Password Safari Extension" 1569813296
-    open -a Safari
-    pausethescript "Open Safari Settings, and in the Extensions tab, check the box for '1Password for Safari' before continuing"
-fi
 
 #
 # Antidote
